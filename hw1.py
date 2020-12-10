9491a4bf4bb75a2fbfb37d78bd83bee5a6e85ba2
garusp1k <garusp1k@mail.ru>
2020-12-10 13:47:26 +0300

    Completed homework

--- hw1.py
+++ hw1.py
@@ -2,80 +2,78 @@
 # запросите у пользователя несколько чисел и строк и сохраните в переменные,
 # выведите на экран.
 
-# author_name = 'Александр'
-# author_location = 'на Сахалине'
-# author_location_square = '76 400 км²'
-# author_location_population = 488257
-#
-# print(f'Добрый день, меня зовут {author_name} и я живу {author_location}.\n'
-#       f'Сахалин крупнейший остров России. Его площадь составляет {author_location_square}')
-# user_name = input('А как зовут Вас?\n>>>')
-#
-# print(f'Приятно познакомиться {user_name}')
-#
-# user_location = input('Где вы проживаете?\n>>>')
-# user_location_population = input('Численность населения в Вашем регионе?\n>>>')
-#
-# if user_location_population.isdigit():
-#     user_location_population = int(user_location_population)
-# else:
-#     print(user_name, 'укажите, пожалуйста, количество людей в числовом формате')
-#     exit()
-#
-# if author_location_population > user_location_population:
-#     print(f'{author_location} живет больше людей')
-# elif author_location_population == user_location_population:
-#     print('Удивительное совпадение!')
-# else:
-#     print(f'В {user_location} людей больше чем {author_location}')
+author_name = 'Александр'
+author_location = 'на Сахалине'
+author_location_square = '76 400 км²'
+author_location_population = 488257
+
+print(f'Добрый день, меня зовут {author_name} и я живу {author_location}.\n'
+      f'Сахалин крупнейший остров России. Его площадь составляет {author_location_square}')
+user_name = input('А как зовут Вас?\n>>>')
+
+print(f'Приятно познакомиться {user_name}')
+
+user_location = input('Где вы проживаете?\n>>>')
+user_location_population = input('Численность населения в Вашем регионе?\n>>>')
+
+if user_location_population.isdigit():
+    user_location_population = int(user_location_population)
+else:
+    print(user_name, 'укажите, пожалуйста, количество людей в числовом формате')
+    exit()
+
+if author_location_population > user_location_population:
+    print(f'{author_location} живет больше людей')
+elif author_location_population == user_location_population:
+    print('Удивительное совпадение!')
+else:
+    print(f'В {user_location} людей больше чем {author_location}')
 
 
 # 2. Пользователь вводит время в секундах. Переведите время в часы, минуты и секунды
 # и выведите в формате чч:мм:сс. Используйте форматирование строк.
 
-# time = input('Введите время в секундах: ')
-# duration_hour = 3600
-# duration_minutes = 60
-#
-# if time.isdigit():
-#     time = int(time)
-# else:
-#     print('Укажите, пожалуйста, время в числовом формате')
-#     exit()
-#
-# hour = time // duration_hour
-#
-# if time >= duration_hour:
-#     minutes = int(abs(time - duration_hour) / duration_minutes)
-# else:
-#     minutes = time//duration_minutes
-#
-# # minutes = int(abs(time - duration_hour) / duration_minutes)  # не понятен перевод в минуты
-# second = (time - duration_hour) % duration_minutes
-#
-# print(f'При вводе {time} секунд получается:\n{hour}: {minutes}: {second}')
+time = input('Введите время в секундах: ')
+duration_hour = 3600
+duration_minutes = 60
+
+if time.isdigit():
+    time = int(time)
+else:
+    print('Укажите, пожалуйста, время в числовом формате')
+    exit()
+
+hour = time // duration_hour
+
+if time >= duration_hour:
+    minutes = int(abs(time - duration_hour) / duration_minutes)
+else:
+    minutes = time//duration_minutes
+second = (time - duration_hour) % duration_minutes
+
+print(f'При вводе {time} секунд получается:\n{hour}: {minutes}: {second}')
 
 
 # 3. Узнайте у пользователя число n. Найдите сумму чисел n + nn + nnn.
 # Например, пользователь ввёл число 3. Считаем 3 + 33 + 333 = 369.
 
 # Простейший способ
-# number_version_1 = int(input('Введите число N: '))
-# summ_version_1 = (number_version_1 + (number_version_1 * 11) + (number_version_1 * 111))
-# print('Версия первая: ', summ_version_1)
-#
-# # Так как при делении суммы чисел на N при любом раскладе получается 123
-# number_version_2 = int(input('Введите число N: '))
-# summ_version_2 = (number_version_2 * 123)
-# print('Версия вторая: ', summ_version_2)
-#
-# # Корректный способ
-# one_number = int(input('Введите число N: '))
-# step = str(one_number)
-# double_number = step + step
-# triple_number = step + step + step
-# summ_version_3 = one_number + int(double_number) + int(triple_number)
-# print('Версия третья: ', summ_version_3)
+number_version_1 = int(input('Введите число N: '))
+summ_version_1 = (number_version_1 + (number_version_1 * 11) + (number_version_1 * 111))
+print('Версия первая: ', summ_version_1)
+
+# Так как при делении суммы чисел на N при любом раскладе получается 123
+number_version_2 = int(input('Введите число N: '))
+summ_version_2 = (number_version_2 * 123)
+print('Версия вторая: ', summ_version_2)
+
+# Корректный способ
+one_number = int(input('Введите число N: '))
+step = str(one_number)
+double_number = step + step
+triple_number = step + step + step
+summ_version_3 = one_number + int(double_number) + int(triple_number)
+print('Версия третья: ', summ_version_3)
 
 # 4. Пользователь вводит целое положительное число. Найдите самую большую цифру в числе.
 # Для решения используйте цикл while и арифметические операции
@@ -92,8 +90,35 @@ while number>=1:
         print('Максимальная цифра в числе: ', max)
         break
 
-
 # 5. Запросите у пользователя значения выручки и издержек фирмы. Определите, с каким финансовым результатом работает
 # фирма (прибыль — выручка больше издержек, или убыток — издержки больше выручки). Выведите соответствующее сообщение.
 # Если фирма отработала с прибылью, вычислите рентабельность выручки (соотношение прибыли к выручке).
 # Далее запросите численность сотрудников фирмы и определите прибыль фирмы в расчете на одного сотрудника.
+
+proceeds = int(input('Введите значение выручки: '))
+costs = int(input('Введите значение издержки: '))
+
+if proceeds > costs:
+    print('Организация получает прибыль')
+    profitability = proceeds / costs
+    print(f'Рентабельность составляет: {profitability}')
+    number_staff = int(input('Введите количество персонала, работающего в Вашей организации: '))
+    proceeds_staff = proceeds / number_staff
+    print(f'Прибыль организации в расчете на одного сотрудника составляет: {proceeds_staff}')
+elif proceeds == costs:
+    print('Необходимо увеличить производственные мощности!')
+else:
+    print('Организация работает в убыток')
+
+# 6. Спортсмен занимается ежедневными пробежками. В первый день его результат составил a километров. Каждый день
+# спортсмен увеличивал результат на 10 % относительно предыдущего. Требуется определить номер дня, на который общий
+# результат спортсмена составить не менее b километров. Программа должна принимать значения параметров a и b и выводить
+# одно натуральное число — номер дня.
+
+starting_distance = int(input('Сколько спортсмен пробежал километров в первый день?\n>>> '))
+finish_distance = int(input('Сколько километров должен пробежать спортсмен?\n>>> '))
+starting_day = 1
+while finish_distance - starting_distance > 0:
+    starting_distance = starting_distance + (0.1 * starting_distance)
+    starting_day += 1
+print(f'Спортсмен пробежит {finish_distance} километров на {starting_day} день')
